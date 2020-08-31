4f45f1eb4a5184994bec19d48552f5871989a3e6
New User <dawid.debowski.20@gmail.com>
2020-08-31 10:48:57 +0200

    Creating an Empty Application and added dependencies

---
+++ Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.raiseyourglass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
