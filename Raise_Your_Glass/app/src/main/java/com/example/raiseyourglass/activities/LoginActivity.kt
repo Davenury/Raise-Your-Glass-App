62298c287a095fb965ceeb81348006e75eac1aa8
New User <dawid.debowski.20@gmail.com>
2020-08-31 12:34:55 +0200

    Project structure changes and MainActivity -> LoginActivity

--- Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/activities/LoginActivity.kt
+++ Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/activities/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.raiseyourglass
+package com.example.raiseyourglass.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.raiseyourglass.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
