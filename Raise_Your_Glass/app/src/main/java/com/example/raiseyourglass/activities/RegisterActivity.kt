ac5c967b95cc1ba80e436c67559c09f7c51ac408
Rados13 <radoslawszuma@gmail.com>
2020-09-01 11:45:11 +0200

    Start activity and empty rest activities

--- Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/activities/RegisterActivity.kt
+++ Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/activities/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.example.raiseyourglass.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.raiseyourglass.R
 import com.example.raiseyourglass.firebase.Firebase
@@ -16,19 +17,24 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        btnRegister.setOnClickListener{
+        Firebase.setContext(applicationContext)
+        btnRegister.setOnClickListener {
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
+            println("$email, $password")
             Firebase.registerWithEmailAndPassword(email, password)
-
+            checkIfLogged()
         }
     }
 
-    private fun checkIfLogged(){
-        if(Firebase.isUserLogged()){
-            Intent()
-        }else{
-            Toast.makeText(this.applicationContext, "Registration failed", Toast.LENGTH_LONG).show()
+    private fun checkIfLogged() {
+        if (Firebase.isUserLogged()) {
+            Intent(this, StartActivity::class.java).also {
+                startActivity(it)
+            }
+        } else {
+//            Toast.makeText(this.applicationContext, "Registration failed", Toast.LENGTH_LONG).show()
+            Log.d("register", "We are fucked up")
         }
     }
 }
