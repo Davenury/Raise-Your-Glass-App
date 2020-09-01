16ed9880202303d23379e831494e913d40ff86c6
Rados13 <radoslawszuma@gmail.com>
2020-09-01 10:44:29 +0200

    Register and Login Activity

--- Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/activities/RegisterActivity.kt
+++ Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/activities/RegisterActivity.kt
@@ -1,39 +1,33 @@
 package com.example.raiseyourglass.activities
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.raiseyourglass.R
 import com.example.raiseyourglass.firebase.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.btnRegister
+import kotlinx.android.synthetic.main.activity_register.etEmail
+import kotlinx.android.synthetic.main.activity_register.etPassword
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        Firebase.setContext(applicationContext)
-
-        btnLogin.setOnClickListener {
+        setContentView(R.layout.activity_register)
+        btnRegister.setOnClickListener{
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
-            Firebase.loginWithEmailAndPassword(email, password)
-            checkLogged()
-        }
+            Firebase.registerWithEmailAndPassword(email, password)
 
-        btnRegister.setOnClickListener{
-            Intent(this,RegisterActivity::class.java)
         }
     }
 
-    private fun checkLogged(){
+    private fun checkIfLogged(){
         if(Firebase.isUserLogged()){
-            tvLoggedIn.text = "Logged in"
             Intent()
-        }
-        else{
-            tvLoggedIn.text = "Not logged in"
+        }else{
+
         }
     }
 }
