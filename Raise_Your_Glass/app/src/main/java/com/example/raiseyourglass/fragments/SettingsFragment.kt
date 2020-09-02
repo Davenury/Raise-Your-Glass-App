fc2a15bc8ab8349319c4b67c1b917d58b9ffdb0b
Rados13 <radoslawszuma@gmail.com>
2020-09-02 10:00:32 +0200

    SettingsFragment

--- Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/fragments/SettingsFragment.kt
+++ Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/fragments/SettingsFragment.kt
@@ -6,7 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.raiseyourglass.R
+import com.example.raiseyourglass.firebase.Firebase
+import kotlinx.android.synthetic.main.fragment_settings.*
 
-class SettingsFragment : Fragment(R.layout.fragment_settings) {
+class SettingsFragment(val finishActivity: () -> Unit) : Fragment(R.layout.fragment_settings) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        etNewName.setText(Firebase.getUserName() ?: "")
+
+        btnLogout.setOnClickListener{
+            Firebase.logout()
+            finishActivity()
+        }
+
+        btnSaveChangedName.setOnClickListener{
+            val newName = etNewName.text.toString()
+            Firebase.updateUserName(newName)
+        }
+    }
 
 }
