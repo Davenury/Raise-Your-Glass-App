76635a6e8d6d296753f175139691ed864561648b
Rados13 <radoslawszuma@gmail.com>
2020-09-04 12:19:38 +0200

    Adding new drinks and app logic for updateting drinks

--- Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/adapters/StepsListAdapter.kt
+++ Raise_Your_Glass/app/src/main/java/com/example/raiseyourglass/adapters/StepsListAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.raiseyourglass.adapters
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,14 +11,17 @@ import com.example.raiseyourglass.dataclasses.Step
 import kotlinx.android.synthetic.main.item_ingredients_list_modification.view.*
 import kotlinx.android.synthetic.main.item_steps_list_modification.view.*
 
-class StepsListAdapter(val stepsList: MutableList<Step>, val removeStep: (position:Int) -> Unit) :
+class StepsListAdapter(val stepsList: MutableList<Step>, val removeStep: (position: Int) -> Unit) :
     RecyclerView.Adapter<StepsListAdapter.StepsListViewHolder>() {
 
     inner class StepsListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
+    val changedData: MutableList<Step> = stepsList
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StepsListViewHolder {
         val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_steps_list_modification, parent, false)
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_steps_list_modification, parent, false)
         return StepsListViewHolder(view)
     }
 
@@ -24,6 +29,17 @@ class StepsListAdapter(val stepsList: MutableList<Step>, val removeStep: (positi
         holder.itemView.apply {
             etStep.setText(stepsList[position].name)
         }
+
+        holder.itemView.etStep.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                changedData[position].name = s.toString()
+            }
+
+        })
 
         holder.itemView.fabtnDeleteStep.setOnClickListener {
             removeStep(position)
